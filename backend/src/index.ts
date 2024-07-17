fbbc100f1a54ba937b51a68ca54a5583fe912ac2
LearnRK <learn.rk10101@gmail.com>
2024-07-17 18:28:00 +0530

    frontend signup and signin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
@@ -13,20 +14,14 @@ const app = new Hono<{
   }
 }>();
 
+app.use('/api/*', cors())
+
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
 
 export default app
  
 
-/* postgress
-
-postgresql://DBase_1_owner:H2YqolJteg0s@ep-steep-frost-a1mpzhk8.ap-southeast-1.aws.neon.tech/DBase_1?sslmode=require
-*/
-
-/*
-DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiOThkZWZjZGItNGE0YS00MGU0LWJhNTYtMjExODEyYzRkMmIyIiwidGVuYW50X2lkIjoiMmJjYzhiMzYwYTkzN2ExNzU2YTJkZjE3MTI2MWRiNTY2ZDRhM2MyYWM4NDM1MTZjYzc3ZGUzYjk1N2VkZTJjNyIsImludGVybmFsX3NlY3JldCI6ImYxYzlhNWQ4LWYwZjQtNDVjOC1iMjZhLWExY2M0MTcxMDQyOSJ9.cgBpfkxl6WMuNiNnXDpN9msCpRFvbu2PI3jOGfXeO2U"
-*/
 
 
 // Deployed on cloudflare worker
---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,18 @@
+
+export const Quote = () =>{
+    return <div className="bg-slate-200 h-screen flex justify-center">
+        <div className="flex justify-center items-center">
+            <div className="flex-block justify-center items-center ">
+                <div className="max-w-lg text-2xl font-bold ">
+                    "The customer service I recieved was exceptional. The support team went above and beyond to address my concerns"
+                </div>
+                <div className="text-lg">
+                    Jules Winnfeild
+                </div>
+                <div className="text-sm text-slate-50">
+                    CEO | Acme Inc
+                </div>
+            </div>
+        </div>
+    </div>
+}
